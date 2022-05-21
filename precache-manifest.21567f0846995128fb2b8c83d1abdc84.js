11ddacec22835cb5a66f1952fab0c65093ac4179
Binary-BeastByte <legionanynomous@gmail.com>
2022-05-21 23:09:10 +0545

    Updates

---
+++ precache-manifest.21567f0846995128fb2b8c83d1abdc84.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9b40d8264cff3b7f1f",
+    "url": "/cryptopay/static/css/main.0b04fbac.chunk.css"
+  },
+  {
+    "revision": "6d9b40d8264cff3b7f1f",
+    "url": "/cryptopay/static/js/main.cf1f3015.chunk.js"
+  },
+  {
+    "revision": "67739f3b691b12b7aa3b",
+    "url": "/cryptopay/static/js/runtime~main.d1eb147f.js"
+  },
+  {
+    "revision": "223428e594dd45c5f199",
+    "url": "/cryptopay/static/js/2.24657b96.chunk.js"
+  },
+  {
+    "revision": "3d66be6d13225e756803c62ef4e6a78b",
+    "url": "/cryptopay/static/media/svg-1.3d66be6d.svg"
+  },
+  {
+    "revision": "8581aab9c4d531aa689a763f3dfc0115",
+    "url": "/cryptopay/static/media/svg-2.8581aab9.svg"
+  },
+  {
+    "revision": "00157fe0db661f6b61ec530858bf6f83",
+    "url": "/cryptopay/static/media/svg-3.00157fe0.svg"
+  },
+  {
+    "revision": "d7e61dad7e378a7c619b910bece2a0b4",
+    "url": "/cryptopay/static/media/video.d7e61dad.mp4"
+  },
+  {
+    "revision": "13e1520b4fdb50dc58a6c39678efc533",
+    "url": "/cryptopay/index.html"
+  }
+];
